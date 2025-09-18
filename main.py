6d5067f3532e860e65cc3f44354b3859e373e92e
ethanmcarr <cethan95@gmail.com>
2025-09-17 21:52:49 -0400

    creating stats.py

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
+from stats import get_number_of_words
+
 def get_book_text(book_file_path):
     with open(book_file_path) as book_file:
         book_contents = book_file.read()
     return book_contents
-
-def get_number_of_words(book_text):
-    array_of_words = book_text.split()
-    return len(array_of_words)
 
 def main():
     book_text = get_book_text("books/frankenstein.txt")
---
+++ stats.py
@@ -0,0 +1,3 @@
+def get_number_of_words(book_text):
+    array_of_words = book_text.split()
+    return len(array_of_words)
