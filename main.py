3aa229945cc5c2740fd380beb53a4f92ffeaa2a4
ethanmcarr <cethan95@gmail.com>
2025-09-17 22:57:16 -0400

    adding indepth print output

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from stats import get_number_of_words
+from stats import get_character_dictionary
+from stats import get_sorted_list_of_dictionaries
+import sys
 
 def get_book_text(book_file_path):
     with open(book_file_path) as book_file:
@@ -6,8 +9,23 @@ def get_book_text(book_file_path):
     return book_contents
 
 def main():
-    book_text = get_book_text("books/frankenstein.txt")
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_file_path = sys.argv[1]
+    book_text = get_book_text(book_file_path)
     number_of_words = get_number_of_words(book_text)
+    dictionary_of_words = get_character_dictionary(book_text)
+    character_dictionary_list = list(get_sorted_list_of_dictionaries(dictionary_of_words))
     print(f"{ number_of_words } words found in the document")
+    print()
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at { book_file_path }...")
+    print("----------- Word Count ----------")
+    print(f"Found { number_of_words } total words")
+    print("--------- Character Count -------")
+    for i in character_dictionary_list:
+        print(f"{ i["character"] }: { i["count"] }")
+    print("============= END ===============")
 
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,27 @@
+def character_sort_on(character):
+    return character["count"]
+
 def get_number_of_words(book_text):
     array_of_words = book_text.split()
     return len(array_of_words)
+
+def get_character_dictionary(book_text):
+    lowercase_book_text = book_text.lower()
+    character_set = set(lowercase_book_text)
+    character_dictionary = {}
+    for character in character_set:
+        character_dictionary[character] = lowercase_book_text.count(character)
+    return character_dictionary
+
+def get_sorted_list_of_dictionaries(character_dictionary):
+    list_of_dictionaries = list()
+    for character in character_dictionary:
+        if character.isalpha():
+            list_of_dictionaries.append(
+                {
+                    "character": character,
+                    "count": character_dictionary[character]
+                }
+            )
+    list_of_dictionaries.sort(reverse=True, key=character_sort_on)
+    return list_of_dictionaries
